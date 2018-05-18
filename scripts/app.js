043b133e8443adcf75f1ddd41f3563f9780aa056
Michael Lauridsen <Pjuker@gmail.com>
2018-05-18 22:33:55 +0200

    Added styling

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,5 @@
+let state;
+
 function writeNewItem(name, measurement, quantity) {
     // A post entry.
     var postData = {
@@ -55,14 +57,27 @@ function refreshUI(list) {
     for (var i = 0; i < list.length; i++) {
         wares += `
             <div data-item-key="${list[i].key}" class="shopping-list-ware">
-                <p>${list[i].quantity} ${list[i].measurement} ${list[i].name}</p>
-                <button class="btn btn-edit"> Edit </button>
-                <button class="btn btn-delete"> Delete </button>
+                <div class="shopping-list-ware__content">
+                    <p><strong>${list[i].name}</strong> ${list[i].quantity} ${list[i].measurement} </p>
+                </div>
+                <div class="shopping-list-ware__button-group">
+                    <button class="btn btn-edit"> 
+                        <svg class="edit__icon">
+                            <use xlink:href="./sprite.svg#icon-pencil" />
+                        </svg>
+                     </button>
+                    <button class="btn btn-delete"> 
+                        <svg class="delete__icon">
+                            <use xlink:href="./sprite.svg#icon-trash" />
+                        </svg>
+                    </button>
+                </div>
             </div>
             `;
     };
     document.getElementById('shoppingList').innerHTML = wares;
     updateListeners();
+    return true;
 };
 
 var ref = firebase.database().ref("items");
@@ -75,6 +90,7 @@ ref.on("value", function (snapshot) {
         items.push(childData);
     })
     console.log(items)
+    state = items;
     refreshUI(items)
 })
 
@@ -84,10 +100,10 @@ ref.on("child_removed", function (data) {
 })
 
 function updateListeners() {
-    const editButtons = document.querySelectorAll('.shopping-list-ware .btn-edit');
+    const editButtons = document.querySelectorAll('.shopping-list-ware .shopping-list-ware__button-group .btn-edit');
     Array.from(editButtons).forEach(ware => {
         ware.addEventListener('click', (event) => {
-            const wareKey = event.target.parentElement.attributes["data-item-key"].nodeValue;
+            const wareKey = event.target.parentElement.parentElement.attributes["data-item-key"].nodeValue;
             editSingleWare(event, wareKey);
             getSingleWare(wareKey);
         })
@@ -95,7 +111,7 @@ function updateListeners() {
     const deleteButtons = document.querySelectorAll('.shopping-list-ware .btn-delete');
     Array.from(deleteButtons).forEach(ware => {
         ware.addEventListener('click', (event) => {
-            const wareKey = event.target.parentElement.attributes["data-item-key"].nodeValue;
+            const wareKey = event.target.parentElement.parentElement.attributes["data-item-key"].nodeValue;
             deleteWareFB(wareKey);
         })
     })
@@ -110,14 +126,26 @@ function getSingleWare(key) {
 function editSingleWare(event, key) {
     var wareRef = firebase.database().ref("items").child(key).once('value').then(snapshot => {
         const item = snapshot.val();
+        let formInput = event.target.parentElement.parentElement;
 
-        let formInput = event.target.parentElement;
+        // Set current measurement as selected
+        let dropdown = ``;
+        var options = ['kg', 'gr', 'l', 'ml', 'pcs'];
+        for (let i = 0; i < options.length; i++ ) {
+            if (options[i] == item.measurement) {
+                dropdown+= `<option value="${options[i]}" selected>${options[i]}</option>`
+            } else {
+                dropdown+= `<option value="${options[i]}">${options[i]}</option>`
+            }
+        }
 
         formInput.innerHTML = `
             <form id="editForm">
                 <input type="text" id="name" placeholder="Name" value="${item.name}"/>
                 <input type="number" id="quantity" placeholder="Quantity" value="${item.quantity}"/>
-                <input type="text" id="measurement" placeholder="Measurement" value="${item.measurement}"/>
+                <select id="measurement">
+                    ${dropdown}
+                </select>
                 <input type="submit">
             </form>
         `
@@ -144,20 +172,23 @@ function deleteWareFB(key) {
 function showToast(key, data) {
     const node = document.createElement("div");
     node.classList.add('popUp')
+    const para = document.createElement("P");
     const textNode = document.createTextNode(`${data.val().name} was removed from your shopping list`);
-    node.appendChild(textNode);
+    para.appendChild(textNode);
+    node.appendChild(para);
     document.querySelector('.popUpContainer').appendChild(node);
     const buttonNode = document.createElement("BUTTON");
     const buttonText = document.createTextNode("Undo");
     buttonNode.appendChild(buttonText);
     node.appendChild(buttonNode);
 
+    // Undo / Resave data
     buttonNode.addEventListener('click', () => {
         firebase.database().ref("items").child(key).set(data.val());
         node.style.opacity = '0';
         setTimeout(() => {
             node.remove();
-        }, 1000)
+        }, 300)
     })
 
     setTimeout(() => {
@@ -165,5 +196,5 @@ function showToast(key, data) {
         setTimeout(() => {
             node.remove();
         }, 300)
-    }, 3000)
+    }, 5000)
 }
